a0fee770436d47289544f54d6984fed9daa3807f
LarTekmeng <106417236+LarTekmeng@users.noreply.github.com>
2025-10-14 16:17:10 +0700

    latest

--- node_api/db.js
+++ node_api/db.js
@@ -8,15 +8,15 @@ pg.types.setTypeParser(1700, (v) => (v == null ? null : parseFloat(v)));
 
 const pgp = require('pg-promise')();
 
-const cn =
-  //process.env.DATABASE_URL ||
-  {
-    host: process.env.DB_HOST,           // should be "db"
-    port: Number(process.env.DB_PORT || 5432),
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-  };
+const cn = process.env.DATABASE_URL
+  ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+  : {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT || 5432),
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+    };
 
 const db = pgp(cn);
 module.exports = db;
